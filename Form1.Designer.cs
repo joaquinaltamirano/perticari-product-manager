712c8eb2efa27e98fa67f9bd4dda7f34169cd0f7
Joaquín Altamirano <joaquin.altamirano.j@gmail.com>
2026-04-01 19:12:51 -0300

    intial UI layout (no logic yet)

---
+++ Form1.Designer.cs
@@ -0,0 +1,446 @@
+﻿namespace WinFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            XX = new Label();
+            label1 = new Label();
+            linkLabel1 = new LinkLabel();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            bloquePrincipal = new Panel();
+            panel4 = new Panel();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            panel5 = new Panel();
+            flowLayoutPanel3 = new FlowLayoutPanel();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            panel6 = new Panel();
+            label3 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            bloquePrincipal.SuspendLayout();
+            panel4.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            panel5.SuspendLayout();
+            flowLayoutPanel3.SuspendLayout();
+            panel6.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(dataGridView1);
+            panel1.Location = new Point(422, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(336, 495);
+            panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(22, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Breadcrumb";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(22, 41);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(293, 423);
+            dataGridView1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(linkLabel1);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(flowLayoutPanel1);
+            panel2.Location = new Point(60, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(336, 495);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.AutoScroll = true;
+            panel3.Controls.Add(panel5);
+            panel3.Controls.Add(bloquePrincipal);
+            panel3.Location = new Point(19, 177);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(293, 287);
+            panel3.TabIndex = 5;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.Location = new Point(169, 51);
+            button6.Name = "button6";
+            button6.Size = new Size(77, 42);
+            button6.TabIndex = 11;
+            button6.Text = "FERRETERÍA";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.Location = new Point(86, 51);
+            button7.Name = "button7";
+            button7.Size = new Size(77, 42);
+            button7.TabIndex = 10;
+            button7.Text = "HYS";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.Location = new Point(3, 51);
+            button8.Name = "button8";
+            button8.Size = new Size(77, 42);
+            button8.TabIndex = 9;
+            button8.Text = "OBRA";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(3, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(77, 42);
+            button5.TabIndex = 8;
+            button5.Text = "PERFILES";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(169, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(77, 42);
+            button4.TabIndex = 7;
+            button4.Text = "CHAPAS";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(86, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(77, 42);
+            button3.TabIndex = 2;
+            button3.Text = "CAÑOS";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // XX
+            // 
+            XX.AutoSize = true;
+            XX.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            XX.Location = new Point(3, 7);
+            XX.Name = "XX";
+            XX.Size = new Size(102, 21);
+            XX.TabIndex = 6;
+            XX.Text = "PRODUCTOS";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(19, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 21);
+            label1.TabIndex = 4;
+            label1.Text = "16:35";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            linkLabel1.ForeColor = SystemColors.ControlText;
+            linkLabel1.Location = new Point(252, 13);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(60, 20);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Limpiar";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(262, 128);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 27);
+            button2.TabIndex = 2;
+            button2.Text = "Buscar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(19, 128);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(293, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Location = new Point(19, 44);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(293, 67);
+            flowLayoutPanel1.TabIndex = 0;
+            flowLayoutPanel1.Paint += flowLayoutPanel1_Paint;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 44);
+            button1.TabIndex = 0;
+            button1.Text = "←";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // bloquePrincipal
+            // 
+            bloquePrincipal.BorderStyle = BorderStyle.FixedSingle;
+            bloquePrincipal.Controls.Add(flowLayoutPanel2);
+            bloquePrincipal.Controls.Add(panel4);
+            bloquePrincipal.Location = new Point(6, 5);
+            bloquePrincipal.Name = "bloquePrincipal";
+            bloquePrincipal.Size = new Size(275, 141);
+            bloquePrincipal.TabIndex = 12;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(XX);
+            panel4.Location = new Point(2, 3);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(267, 34);
+            panel4.TabIndex = 13;
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Controls.Add(button5);
+            flowLayoutPanel2.Controls.Add(button3);
+            flowLayoutPanel2.Controls.Add(button4);
+            flowLayoutPanel2.Controls.Add(button8);
+            flowLayoutPanel2.Controls.Add(button7);
+            flowLayoutPanel2.Controls.Add(button6);
+            flowLayoutPanel2.Location = new Point(2, 38);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(267, 98);
+            flowLayoutPanel2.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            panel5.BorderStyle = BorderStyle.FixedSingle;
+            panel5.Controls.Add(flowLayoutPanel3);
+            panel5.Controls.Add(panel6);
+            panel5.Location = new Point(3, 264);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(275, 141);
+            panel5.TabIndex = 14;
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.Controls.Add(button9);
+            flowLayoutPanel3.Controls.Add(button10);
+            flowLayoutPanel3.Controls.Add(button11);
+            flowLayoutPanel3.Controls.Add(button12);
+            flowLayoutPanel3.Controls.Add(button13);
+            flowLayoutPanel3.Controls.Add(button14);
+            flowLayoutPanel3.Location = new Point(2, 38);
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            flowLayoutPanel3.Size = new Size(267, 98);
+            flowLayoutPanel3.TabIndex = 2;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.Location = new Point(3, 3);
+            button9.Name = "button9";
+            button9.Size = new Size(77, 42);
+            button9.TabIndex = 8;
+            button9.Text = "PERFILES";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button10.Location = new Point(86, 3);
+            button10.Name = "button10";
+            button10.Size = new Size(77, 42);
+            button10.TabIndex = 2;
+            button10.Text = "CAÑOS";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button11.Location = new Point(169, 3);
+            button11.Name = "button11";
+            button11.Size = new Size(77, 42);
+            button11.TabIndex = 7;
+            button11.Text = "CHAPAS";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button12.Location = new Point(3, 51);
+            button12.Name = "button12";
+            button12.Size = new Size(77, 42);
+            button12.TabIndex = 9;
+            button12.Text = "OBRA";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button13.Location = new Point(86, 51);
+            button13.Name = "button13";
+            button13.Size = new Size(77, 42);
+            button13.TabIndex = 10;
+            button13.Text = "HYS";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button14.Location = new Point(169, 51);
+            button14.Name = "button14";
+            button14.Size = new Size(77, 42);
+            button14.TabIndex = 11;
+            button14.Text = "FERRETERÍA";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(label3);
+            panel6.Location = new Point(2, 3);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(267, 34);
+            panel6.TabIndex = 13;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(3, 7);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 21);
+            label3.TabIndex = 6;
+            label3.Text = "PRODUCTOS";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 521);
+            Controls.Add(button1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            bloquePrincipal.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            flowLayoutPanel2.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            flowLayoutPanel3.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private LinkLabel linkLabel1;
+        private Label label1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private Panel panel3;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Label XX;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private Panel bloquePrincipal;
+        private Panel panel4;
+        private Panel panel5;
+        private FlowLayoutPanel flowLayoutPanel3;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Panel panel6;
+        private Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
